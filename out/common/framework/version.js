3ac1709ca2b9ee01a328f35ac94ea9ee3d5b2529
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-10 17:40:38 +0000

    Deploy gpuweb/cts to github.com/gpuweb/cts.git:gh-pages

---
+++ out/common/framework/version.js
@@ -0,0 +1,3 @@
+// AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
+
+export const version = '0370fbb590bf17dfee8bb8fd4e26644f37f49165';
---
+++ out/webgpu/listing.js
@@ -0,0 +1,322 @@
+// AUTO-GENERATED - DO NOT EDIT. See src/common/tools/gen_listings.ts.
+
+export const listing = [
+  {
+    "file": [],
+    "readme": "WebGPU conformance test suite."
+  },
+  {
+    "file": [
+      "api"
+    ],
+    "readme": "Tests for full coverage of the Javascript API surface of WebGPU."
+  },
+  {
+    "file": [
+      "api",
+      "operation"
+    ],
+    "readme": "Tests that check the result of performing valid WebGPU operations, taking advantage of\nparameterization to exercise interactions between features."
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "buffers"
+    ],
+    "readme": "GPUBuffer tests."
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "buffers",
+      "create_mapped"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "buffers",
+      "map"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "buffers",
+      "map_detach"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "buffers",
+      "map_oom"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "command_buffer",
+      "basic"
+    ],
+    "description": "Basic tests."
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "command_buffer",
+      "copies"
+    ],
+    "description": "copy{Buffer,Texture}To{Buffer,Texture} tests."
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "command_buffer",
+      "render",
+      "basic"
+    ],
+    "description": "Basic command buffer rendering tests."
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "fences"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "render_pass",
+      "storeOp"
+    ],
+    "description": "API Operation Tests for RenderPass StoreOp.\n\n  Test Coverage Needed:\n\n  - Test that a render pass has correct output for combinations of:\n    - All color attachments from '0' to 'MAX_COLOR_ATTACHMENTS' with combinations of:\n      - storeOp set to {'clear', 'store', 'undefined}\n      - All color renderable formats\n      - mip level set to {'0', mip > '0'}\n      - array layer set to {'0', layer > '1'} for 2D textures\n      - depth slice set to {'0', slice > '0'} for 3D textures\n    - With and without a depthStencilAttachment that has the combinations of:\n      - depthStoreOp set to {'clear', 'store', 'undefined'}\n      - stencilStoreOp set to {'clear', 'store', 'undefined'}\n      - All depth/stencil formats\n      - mip level set to {'0', mip > '0'}\n      - array layer set to {'0', layer > '1'} for 2D textures\n      - depth slice set to {'0', slice > '0'} for 3D textures"
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "resource_init",
+      "copied_texture_clear"
+    ],
+    "description": "Test uninitialized textures are initialized to zero when copied."
+  },
+  {
+    "file": [
+      "api",
+      "regression"
+    ],
+    "readme": "One-off tests that reproduce API bugs found in implementations to prevent the bugs from\nappearing again."
+  },
+  {
+    "file": [
+      "api",
+      "validation"
+    ],
+    "readme": "Positive and negative tests for all the validation rules of the API."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "createBindGroup"
+    ],
+    "description": "createBindGroup validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "createBindGroupLayout"
+    ],
+    "description": "createBindGroupLayout validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "createPipelineLayout"
+    ],
+    "description": "createPipelineLayout validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "createTexture"
+    ],
+    "description": "createTexture validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "createView"
+    ],
+    "description": "createView validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "error_scope"
+    ],
+    "description": "error scope validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "fences"
+    ],
+    "description": "fences validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "queue_submit"
+    ],
+    "description": "queue submit validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "render_pass",
+      "storeOp"
+    ],
+    "description": "API Validation Tests for RenderPass StoreOp.\n\n  Test Coverage Needed:\n\n  - Test that when depthReadOnly is true, depthStoreOp must be 'store'\n\n  - Test that when stencilReadOnly is true, stencilStoreOp must be 'store'"
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "render_pass_descriptor"
+    ],
+    "description": "render pass descriptor validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "setBindGroup"
+    ],
+    "description": "setBindGroup validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "setBlendColor"
+    ],
+    "description": "setBlendColor validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "setScissorRect"
+    ],
+    "description": "setScissorRect validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "setStencilReference"
+    ],
+    "description": "setStencilReference validation tests."
+  },
+  {
+    "file": [
+      "api",
+      "validation",
+      "setViewport"
+    ],
+    "description": "setViewport validation tests."
+  },
+  {
+    "file": [
+      "examples"
+    ],
+    "description": "Examples of writing CTS tests with various features.\n\nStart here when looking for examples of basic framework usage."
+  },
+  {
+    "file": [
+      "idl"
+    ],
+    "readme": "Tests to check that the WebGPU IDL is correctly implemented, for examples that objects exposed\nexactly the correct members, and that methods throw when passed incomplete dictionaries."
+  },
+  {
+    "file": [
+      "idl",
+      "constants",
+      "flags"
+    ],
+    "description": "Test the values of flags interfaces (e.g. GPUTextureUsage)."
+  },
+  {
+    "file": [
+      "shader"
+    ],
+    "readme": "Tests for full coverage of the shaders that can be passed to WebGPU."
+  },
+  {
+    "file": [
+      "shader",
+      "execution"
+    ],
+    "readme": "Tests that check the result of valid shader execution."
+  },
+  {
+    "file": [
+      "shader",
+      "regression"
+    ],
+    "readme": "One-off tests that reproduce shader bugs found in implementations to prevent the bugs from\nappearing again."
+  },
+  {
+    "file": [
+      "shader",
+      "validation"
+    ],
+    "readme": "Positive and negative tests for all the validation rules of the shading language."
+  },
+  {
+    "file": [
+      "web-platform"
+    ],
+    "readme": "Tests for Web platform-specific interactions like GPUSwapChain and canvas, WebXR,\nImageBitmaps, and video APIs."
+  },
+  {
+    "file": [
+      "web-platform",
+      "canvas",
+      "context_creation"
+    ],
+    "description": ""
+  },
+  {
+    "file": [
+      "web-platform",
+      "copyImageBitmapToTexture"
+    ],
+    "description": "copy imageBitmap To texture tests."
+  }
+];
